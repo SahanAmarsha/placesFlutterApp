9fe2605c9754a3b1649f06abf32e506a3ffb15a2
Sahan Amarsha <sahanamarsha@gmail.com>
2020-05-01 10:56:49 +0530

    Created the app. Finished the layout. Completed on accessing the gallery using Flutter packages.

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,40 @@
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'add_place_screen.dart';
+import '../providers/great_places.dart';
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+          )
+        ],
+      ),
+      body: Consumer<GreatPlaces>(
+        child: Center(
+          child:const Text('Got no places yet start adding some!'),
+        ),
+        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length<=0 ? ch : ListView.builder(
+            itemCount: greatPlaces.items.length,
+            itemBuilder: (ctx, index) => ListTile(
+              leading: CircleAvatar(
+                backgroundImage: FileImage(greatPlaces.items[index].image),
+              ),
+              title: Text(greatPlaces.items[index].title),
+              onTap: () {
+
+              },
+            )
+        ),
+      )
+    );
+  }
+}
